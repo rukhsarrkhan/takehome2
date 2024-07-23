6fff23e09a1bf6faa510a19c0c8da1c0b0281c30
prnvkbr <pranavkuber@gmail.com>
2024-07-23 17:10:28 -0400

    added custom app bar

--- src/components/CustomAppBar.js
+++ src/components/CustomAppBar.js
@@ -1,15 +1,173 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import {Typography} from "@mui/material";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import CustomDivider from "./CustomDivider";
 
+const pages = [{ title: 'My cases', link: '/cases' }];
+const settings = ['Logout'];
+
+function stringAvatar(name) {
+    const initials = name
+        ? `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+        : '';
+    return {
+        sx: {
+            backgroundColor: '#D9D9D9',
+            color: '#484848',
+        },
+        children: initials,
+    };
+}
 
 export default function CustomAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const userName = 'John Doe';
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const handleLogout = () => {
+        console.log('Logout');
+    };
 
     return (
         <AppBar elevation={0} position="static">
-            <Container maxWidth="xl" sx={{padding: 5, display: 'flex', justifyContent: 'center'}}>
-                <Typography variant="h5">New assessment</Typography>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'flex', md: 'none' },
+                        }}
+                    >
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page.title}
+                                    onClick={handleCloseNavMenu}
+                                    component="a"
+                                    href={page.link}
+                                >
+                                    <Typography textTransform="none" textAlign="center">
+                                        {page.title}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    {/* Desktop */}
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: { xs: 'none', md: 'flex' },
+                        }}
+                    >
+                        {pages.map((page) => (
+                            <Button
+                                key={page.title}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    color: '#484848',
+                                    display: 'block',
+                                }}
+                                href={page.link}
+                            >
+                                <Typography
+                                    sx={{ fontWeight: 800, fontSize: 20 }}
+                                    textTransform="none"
+                                >
+                                    {page.title}
+                                </Typography>
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                {/* eslint-disable react/jsx-props-no-spreading */}
+                                <Avatar {...stringAvatar(userName)} />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleLogout}>
+                                    <Typography textTransform="none" textAlign="center">
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+                <CustomDivider />
             </Container>
         </AppBar>
     );
